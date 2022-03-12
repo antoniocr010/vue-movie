0c1a334e89e56447ce3da810c0b376bb74533c0a
Antonio Massaia <86974452+antoniocr010@users.noreply.github.com>
2022-03-11 21:34:38 -0400

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const token =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTU3NzBiN2QxMGE2N2Q2NDQ1NjcxZjRiODM2YWZhYSIsInN1YiI6IjYyMWQwZDVmOTAyMDEyMDA2ZDNiZjlkOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VN1WmftMmqZwDwH8A-9Vk-duujSCm1eE-MGi6YG6KM0';
+  'Token';
 export default axios.create({
   baseURL:'https://api.themoviedb.org/3',
   Headers: {
